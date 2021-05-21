463263ed9a2ca81fd8952712fd48a89285181309
yunhuip <yunhuipengys@gmail.com>
2021-05-21 00:09:39 -0400

    Update sub_job.sh

--- Simulations_set_up/AMBER_OPC/sub_job.sh
+++ Simulations_set_up/AMBER_OPC/sub_job.sh
@@ -11,7 +11,7 @@ module load amber/18
 mpirun  $AMBERHOME/bin/pmemd.MPI -O -i Min.in -o output/Min.out -p 1kx5_amber.prmtop -c 1kx5_amber.inpcrd -r output/Min.ncrst -inf output/Min.mdinfo 
 mpirun  $AMBERHOME/bin/pmemd.MPI -O -i Equil_v.in -o output/Equil_v.out -p 1kx5_amber.prmtop -c output/Min.ncrst -r output/Equil_v.ncrst -x output/Equil_v.nc -inf output/Equil_v.mdinfo
 mpirun  $AMBERHOME/bin/pmemd.MPI -O -i Equil_pt.in -o output/Equil_pt.out -p 1kx5_amber.prmtop -c output/Equil_v.ncrst -r output/Equil_pt.ncrst -x output/Equil_pt.nc -inf output/Equil_pt.mdinfo
-#mpirun  $AMBERHOME/bin/pmemd.MPI -O -i Prod.in -o output/Prod.out -p 1kx5_amber.prmtop -c output/Equil_pt.ncrst -r output/Prod.ncrst -x output/Prod.nc -inf output/Prod.info 
+mpirun  $AMBERHOME/bin/pmemd.MPI -O -i Prod.in -o output/Prod.out -p 1kx5_amber.prmtop -c output/Equil_pt.ncrst -r output/Prod.ncrst -x output/Prod.nc -inf output/Prod.info 
 
 #submit job
 #sbatch --partition=multinode --constraint=x2680 --ntasks=280 --ntasks-per-core=1 --time=168:00:00 --exclusive sub_job.sh
